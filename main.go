11227b4e9c9244ed6f18f800c69fdb354afb5024
Fardin Kamali <fardin.k.s.10.1@gmail.com>
2025-11-06 22:26:14 +0330

    feat:implement 2 route for create and get events and implement sqlite db as database

--- main.go
+++ main.go
@@ -1,21 +1,40 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/FacelessFK/Event_booking/db"
+	"github.com/FacelessFK/Event_booking/models"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	db.InitDB()
 	server := gin.Default()
 
-	server.GET("/", getEvents)
+	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
 
 	server.Run(":8080")
 }
 
 func getEvents(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "Hello!",
-	})
+	events := models.GetAllEvents()
+	ctx.JSON(http.StatusOK, events)
+}
+
+func createEvent(ctx *gin.Context) {
+	var event models.Event
+	err := ctx.ShouldBindJSON(&event)
+	fmt.Println(err)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data."})
+	}
+
+	event.ID = 1
+	event.UserID = 1
+
+	event.Save()
+	ctx.JSON(http.StatusCreated, gin.H{"message": "Event created!", "event": event})
 }
